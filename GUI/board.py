462176e86e490be9cf74365df5ed97c3b3f02260
Krzysztof Zarnowski <kzarnowski7@gmail.com>
2022-09-20 00:40:23 +0200

    board

--- GUI/board.py
+++ GUI/board.py
@@ -1,10 +1,49 @@
-from PyQt5.QtWidgets import QFrame, QVBoxLayout, QLabel
+from re import L
+from PyQt5.QtWidgets import QFrame, QVBoxLayout, QLabel, QWidget, QGridLayout, QPushButton
+from GUI.config import squares_color, theme
 
 class Board(QFrame):
     def __init__(self, parent):
         QFrame.__init__(self)
         self.parent = parent
 
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel('Board'))
-        self.setLayout(layout)
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+        self.layout.setSpacing(0)
+
+        for r in range(8):
+            for c in range(8):
+                #square = Square(self, r, c)
+                square = QWidget(self)
+                #square = Square2(self)
+                if r % 2 == c % 2:
+                    square.setStyleSheet('background-color: #F0D9B5')
+                else:
+                    square.setStyleSheet('background-color: #B58863')
+                self.layout.addWidget(square, r, c)
+
+class Square2(QWidget):
+    def __init__(self, parent):
+        QWidget.__init__(self)
+        self.parent = parent
+
+class Square(QWidget):
+    def __init__(self, parent, r, c):
+        QWidget.__init__(self)
+        self.parent = parent
+        self.r = r
+        self.c = c
+        if r % 2 == c % 2:
+            self.color = squares_color[theme]['dark']
+        else:
+            self.color = squares_color[theme]['light'] 
+        self.setStyleSheet(f'background-color: {self.color}') 
+
+    def mousePressEvent(self, event):
+        print(self.r, self.c)
+        self.parent.parent.header.info.setText(f'{self.r}, {self.c}')
+        
+    # def square_clicked(self):
+    #     print(self.r, self.c)
+    #     self.parent.parent.header.info.setText(f'{self.r}, {self.c}')
+
--- GUI/header.py
+++ GUI/header.py
@@ -1,10 +1,12 @@
-from PyQt5.QtWidgets import QFrame, QHBoxLayout
+from PyQt5.QtWidgets import QFrame, QHBoxLayout, QLabel
 from GUI.menu import MenuButton
 
 class Header(QFrame):
     def __init__(self, parent):
         QFrame.__init__(self)
         self.parent = parent     
+        self.info = QLabel("Info")
         layout = QHBoxLayout()
         layout.addWidget(MenuButton(self.parent.parent))
+        layout.addWidget(self.info)
         self.setLayout(layout)
